e79087fdc7edca95f2842cd757f6f4a058063c6c
沙耶 <yhsia@codersai.com>
2022-04-22 09:01:37 +0800

    db and ef

---
+++ Database/Gender.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE [dbo].[Gender]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Name] NVARCHAR(50) NULL
+)
---
+++ Database/Student.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Student]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Name] NVARCHAR(100) NULL, 
+    [GenderId] INT NULL, 
+    CONSTRAINT [FK_Student_ToGender] FOREIGN KEY ([GenderId]) REFERENCES [Gender]([Id])
+)
